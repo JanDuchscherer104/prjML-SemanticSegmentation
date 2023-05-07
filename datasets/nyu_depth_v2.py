a57431aa02408d78fbd916993b6e6b966d46442f
Jan Duchscherer <duchsche@hm.edu>
2023-05-07 14:31:25 +0200

    adapt dataset, add augmentation

--- datasets/nyu_depth_v2.py
+++ datasets/nyu_depth_v2.py
@@ -1,17 +1,16 @@
 import os
-import random as rnd
+import random
 import sys
+from copy import deepcopy
 
 import h5py
 import numpy as np
-
-# import matplotlib.cm as mplcm
-# import matplotlib.colors as mcolors
 import torch
 from matplotlib import pyplot as plt
 from skimage import color
 from torch.utils.data import Dataset
 
+# os.chdir(os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir))
 sys.path.append("../")
 from utils.transform_2d import RGBDTransform
 
@@ -26,14 +25,15 @@ class NYUDepthV2Dataset(Dataset):
         - add steps in transform pipeline
     """
 
-    __data_dir = os.path.join(os.getcwd(), os.pardir, ".data")
+    __file_dir = os.path.dirname(os.path.abspath(__file__))
+    __data_dir = os.path.join(__file_dir, os.pardir, ".data")
     __set_name = "NYUDepthDatasetV2"
-    __set_file = "nyu_depth_v2_labeled.mat"
+    __set_file = "nyu_depth_v2_masked.mat"
 
     def __init__(
         self,
         data_path=None,
-        split="train",
+        split_type="full",
         transform=None,
         resize=False,
         verbose=True,
@@ -46,48 +46,42 @@ class NYUDepthV2Dataset(Dataset):
             if data_path
             else os.path.join(self.__data_dir, self.__set_name, self.__set_file)
         )
-        self.split = split
+        self.split_type = split_type
         self.transform = transform
         self.verbose = verbose
         self.random_seed = random_seed
-        # rnd.seed(self.random_seed)
-        self.num_samples = num_samples  # TODO remove for training on full dataset
+        # random.seed(self.random_seed)
+        self.num_samples = num_samples  # TODO
 
         # Load the .mat f
         with h5py.File(self.data_path, "r") as f:
             # HxWx3XN -> NxHxWx3
-            # HxWx3xN -> Nx3xHxW TODO
-            self.rgb_images = torch.from_numpy(
-                np.transpose(
-                    f["images"][: self.num_samples].astype(np.float16) / 255.0,
-                    (0, 1, 3, 2),
-                )
+            self.rgb_images = np.transpose(
+                f["images"][: self.num_samples].astype(np.uint8),
+                (0, 3, 2, 1),
             )
-            self.depth_maps = torch.from_numpy(
-                np.transpose(f["depths"][: self.num_samples], (0, 2, 1))
-            )
-            self.labels = torch.from_numpy(
-                np.transpose(
-                    f["labels"][: self.num_samples].astype(np.int64), (0, 2, 1)
-                )
+            self.depth_maps = np.transpose(f["depths"][: self.num_samples], (0, 2, 1))
+            self.masks = np.transpose(
+                f["labels"][: self.num_samples].astype(np.int16), (0, 2, 1)
             )
             chr_arr = [list(f[ref][()].flatten()) for ref in f["names"][0]]
             self.class_names = ["".join(chr(c) for c in name) for name in chr_arr]
 
         if self.transform:
-            size = np.array(self.rgb_images.shape[2:])
+            size = self.rgb_images.shape[1:3]
             if resize:
-                size = (size * resize + 0.5).astype(np.int16)
-            size = tuple(size)
-            std, mean = torch.std_mean(self.rgb_images.reshape(-1, 3), dim=0)  # TODO
-            std_depth, mean_depth = torch.std_mean(
-                self.depth_maps.reshape(-1), dim=0
-            )  # TODO
-            std = std.tolist()
-            mean = mean.tolist()
-            std.append(std_depth.item())
-            mean.append(mean_depth.item())
-            self.transform = RGBDTransform(resize=size, mean=mean, std=std)
+                size = tuple(int(sz * resize + 0.5) for sz in size)
+
+            std = np.std(self.rgb_images.reshape(-1, 3), axis=0).tolist()
+            mean = np.mean(self.rgb_images.reshape(-1, 3), axis=0).tolist()
+            std_depth = np.std(self.depth_maps.reshape(-1)).item()
+            mean_depth = np.mean(self.depth_maps.reshape(-1)).item()
+            std.append(std_depth)
+            mean.append(mean_depth)
+
+            self.transform = RGBDTransform(
+                split_type=self.split_type, resize=size, mean=mean, std=std
+            )
 
     def __len__(self):
         return len(self.rgb_images)
@@ -95,9 +89,9 @@ class NYUDepthV2Dataset(Dataset):
     def __getitem__(self, idx):
         rgb_image = self.rgb_images[idx]
         depth_map = self.depth_maps[idx]
-        label = self.labels[idx]
+        mask = self.masks[idx]
 
-        sample = (rgb_image, depth_map, label)
+        sample = (rgb_image, depth_map, mask)
 
         if self.transform:
             sample = self.transform(sample)
@@ -106,63 +100,63 @@ class NYUDepthV2Dataset(Dataset):
 
     @property
     def num_classes(self):
-        return len(self.class_names)
+        return len(self.class_names) + 1
+
+    @property
+    def rgbd_shape(self):
+        return self[-1][0].shape
+
+    @property
+    def mask_shape(self):
+        return self[-1][1].shape
 
     def __repr__(self) -> str:
         repr = f"""
-        NYUDepthV2Dataset(split={self.split}, data_path={self.data_path})
+        NYUDepthV2Dataset(split={self.split_type}, data_path={self.data_path})
         Number of samples: {len(self)}
-        Number of labels: {len(self.class_names)}
-        RGB image shape: {self.rgb_images.shape}
-        Depth map shape: {self.depth_maps.shape}
-        RGBD sample shape: {self[-1][0].shape}
-        Label shape: {self.labels.shape}"""
+        Number of masks: {len(self.class_names)}
+        RGB image shape: {self.rgb_images.shape}, dtype: {self.rgb_images.dtype}
+        Depth map shape: {self.depth_maps.shape}, dtype: {self.depth_maps.dtype}
+        Mask shape: {self.masks.shape}, dtype: {self.masks.dtype}
+        RGBD sample shape: {self.rgbd_shape}, dtype: {self[-1][0].dtype}
+        Mask shape: {self.mask_shape}, dtype: {self[-1][1].dtype}"""
         return repr
 
-    def visualize_rnd_sample(self, show_norm=False):
-        rnd_idx = rnd.randint(0, len(self) - 1)
+    def visualize_random_sample(self, show_norm=False):
+        random_idx = random.randint(0, len(self) - 1)
         if show_norm:
-            rgbd, label = self.__getitem__(rnd_idx)
-            rgb_image = rgbd[:3:, :, :]
-            depth_map = rgbd[3:, :, :]
+            rgbd, mask = self.__getitem__(random_idx)
+            rgb_image = rgbd[:3:, :, :].cpu().numpy().astype(np.float16) / 255.0
+            depth_map = rgbd[3:, :, :].cpu().numpy()
+            mask = mask.cpu().numpy()
         else:
-            rgb_image = self.rgb_images[rnd_idx]
-            depth_map = self.depth_maps[rnd_idx]
-            label = self.labels[rnd_idx]
+            rgb_image = self.rgb_images[random_idx]
+            depth_map = self.depth_maps[random_idx]
+            mask = self.masks[random_idx]
 
-        rgb_image = rgb_image.numpy().astype(np.float32).transpose(1, 2, 0)
-        depth_map.squeeze_(0)
+        rgb_image = rgb_image.transpose(1, 2, 0).astype(np.float32)
 
-        uique_labels = np.unique(label.flatten().numpy())
-        print(
-            "Found the following labels:\n",
-            [self.label_id2name(e) for e in uique_labels],
-        )
+        # uique_masks = np.unique(mask.flatten())
+        # print(
+        #     "Found the following masks:\n",
+        #     [self.mask_id2name(e) for e in uique_masks],
 
         fig, (ax0, ax1, ax2) = plt.subplots(
             1, 3, sharex=True, sharey=True, figsize=(18, 6)
         )
-        fig.suptitle(f"Sample {rnd_idx} - normalized: {show_norm}")
+        fig.suptitle(f"Sample {random_idx} - normalized: {show_norm}")
         ax0.imshow(rgb_image)
         ax0.set_title("RGB image")
         ax0.set_axis_off()
-        ax1.imshow(depth_map.numpy(), cmap="hot", interpolation="nearest")
+        ax1.imshow(depth_map, cmap="hot", interpolation="nearest")
         ax1.set_title("Depth map")
         ax1.set_axis_off()
-        # im = ax2.imshow(label, cmap=cmap, norm=norm)
-        ax2.imshow(color.label2rgb(label.numpy()))
+        # im = ax2.imshow(mask, cmap=cmap, norm=norm)
+        ax2.imshow(color.mask2rgb(mask))
         ax2.set_title("Ground truth")
         ax2.set_axis_off()
 
-        # colorbar = plt.colorbar(im, ax=ax2, ticks=unique_labels)
-        # colorbar.set_ticklabels([f"{id}:{self.label_id2name(id)}" for id in unique_labels])
-
         plt.show()
-
-    def augment_train_set(self, train_set, num_samples=1000):
-        # TODO how
-        augmented_train_set = train_set
-        return augmented_train_set
 
     def split_dataset(self, split=0.8):
         """Split the dataset into two parts with the specified ratio."""
@@ -176,18 +170,27 @@ class NYUDepthV2Dataset(Dataset):
             [num_samples_train, num_samples_val],
             generator=torch.Generator().manual_seed(self.random_seed),
         )
+        # Set split_type attributes for train_set and val_set
+        train_set.dataset = deepcopy(train_set.dataset)
+        train_set.dataset.split_type = "train"
+        val_set.dataset = deepcopy(val_set.dataset)
+        val_set.dataset.split_type = "val"
         return train_set, val_set
 
-    def label_name2id(self, label_name):
-        return self.class_names.index(label_name)
+    def mask_name2id(self, mask_name):
+        return self.class_names.index(mask_name)
 
-    def label_id2name(self, label_id):
-        return self.class_names[label_id]
+    def mask_id2name(self, mask_id):
+        return self.class_names[mask_id]
 
 
 if __name__ == "__main__":
     dataset = NYUDepthV2Dataset(
-        data_path="/Volumes/Extreme SSD/nyu_depth_v2_labeled.mat"
+        data_path="/Users/janduchscherer/Downloads/nyu_depth_v2_labeled.mat",
+        transform=True,
+        num_samples=12,
+        resize=False,
     )
-    dataset.visualize_rnd_sample()
+    # train_set, val_set = dataset.split_dataset()
+    # train_set.dataset.visualize_random_sample(show_norm=True)
     print(dataset)
--- utils/transform_2d.py
+++ utils/transform_2d.py
@@ -1,47 +1,95 @@
-from typing import Tuple, Union
+from typing import List, Tuple, Union
 
+import albumentations as A
+import cv2
+import numpy as np
 import torch
-import torchvision.transforms as transforms
+from albumentations.pytorch.transforms import ToTensorV2
 
 
 class RGBDTransform:
-    """
-    https://pytorch.org/vision/stable/transforms.html
-    TODO crop,
-    """
+    """ """
 
-    def __init__(self, resize=Union[None, Tuple[int, int]], mean=None, std=None):
-        self.resize = resize
+    def __init__(
+        self,
+        split_type: str,
+        resize: Union[None, Tuple[int, int]],
+        mean: Union[None, List[float]] = None,
+        std: Union[None, List[float]] = None,
+    ):
+        # default: 480, 640 for NYUv2
+        self.split_type = split_type
+        self.height, self.width = resize
         self.mean = mean if mean else [0.485, 0.456, 0.406, 0.5]
         self.std = std if std else [0.229, 0.224, 0.225, 0.5]
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-        self.rgbd_transform = transforms.Compose(
+        self.tf_resize_rgbd = A.Resize(
+            width=self.width, height=self.height, interpolation=cv2.INTER_LINEAR
+        )
+        self.tf_resize_mask = A.Resize(
+            width=self.width, height=self.height, interpolation=cv2.INTER_NEAREST
+        )
+
+        self.tf_augment_rgb = A.Compose(
             [
-                transforms.Resize(
-                    size=self.resize,
-                    interpolation=transforms.InterpolationMode.BILINEAR,
-                    antialias=True,
-                ),
-                transforms.Normalize(mean=self.mean, std=self.std),
+                A.RGBShift(r_shift_limit=15, g_shift_limit=15, b_shift_limit=15, p=0.5),
+                A.RandomBrightnessContrast(p=0.4),
             ]
         )
-        self.label_transform = transforms.Compose(
+        self.tf_augment = A.Compose(
             [
-                transforms.Resize(
-                    # size=self.resize,
-                    size=(120, 160),
-                    interpolation=transforms.InterpolationMode.NEAREST,
-                )
+                A.RandomCrop(width=self.width, height=self.height),
+                A.HorizontalFlip(p=0.5),
+                A.VerticalFlip(p=0.1),
+                A.OneOf(
+                    [
+                        A.Blur(blur_limit=3, p=0.5),
+                        A.ColorJitter(p=0.5),
+                    ],
+                    p=0.8,
+                ),
+                A.RandomScale((-0.2, 0.2), p=0.4),
+                A.RandomRotate90(p=0.3),
+                A.RandomGridShuffle(p=0.2),
+                A.RandomBrightnessContrast(p=0.3),
+            ]
+        )
+        self.tf_normalize = A.Compose(
+            [
+                A.Normalize(mean=self.mean, std=self.std),
+                ToTensorV2(),
+            ]
+        )
+        self.tf_resize_mask_final = A.Compose(
+            [
+                A.Resize(
+                    width=self.width // 4,
+                    height=self.height // 4,
+                    interpolation=cv2.INTER_NEAREST,
+                ),
+                ToTensorV2(),
             ]
         )
 
     def __call__(self, sample):
-        image, depth, label = sample
+        rgb, depth, mask = sample
 
-        # Concatenate the depth channel as the fourth channel 3xhxw, hxw -> 4xhxw
-        rgbd = torch.cat((image, depth.unsqueeze(0)), dim=0)
-        rgbd = self.rgbd_transform(rgbd).to(self.device)
-        label = self.label_transform(label.unsqueeze(0)).squeeze(0).to(self.device)
+        rgb = self.tf_resize_rgbd(image=rgb)["image"]
+        depth = self.tf_resize_rgbd(image=depth)["image"]
+        mask = self.tf_resize_mask(image=mask)["image"]  # use INTER_NEAREST
 
-        return rgbd, label
+        # augment
+        if self.split_type == "train":
+            rgb = self.tf_augment_rgb(image=rgb)["image"]
+            # Concatenate the depth channel as the fourth channel 3xHxW, HxW -> 4xHxW
+            rgbd = np.concatenate((rgb, np.expand_dims(depth, axis=-1)), axis=-1)
+            augmented = self.tf_augment(image=rgbd, mask=mask)
+            rgbd, mask = augmented["image"], augmented["mask"]
+        else:
+            rgbd = np.concatenate((rgb, np.expand_dims(depth, axis=-1)), axis=-1)
+
+        rgbd = self.tf_normalize(image=rgbd)["image"]
+        mask = self.tf_resize_mask_final(image=mask)["image"]
+
+        return rgbd.to(self.device), mask.to(self.device)
